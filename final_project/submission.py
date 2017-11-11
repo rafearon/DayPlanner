42d7ebeb410a2e62d29695717ce9b4ed97dc0b5b
deektionary <deekshagoyal19@gmail.com>
2017-11-11 11:50:26 -0800

    work we did together on csp

---
+++ final_project/submission.py
@@ -0,0 +1,198 @@
+import collections, util, copy
+
+# A class providing methods to generate CSP that can solve the day scheduling
+# problem.
+class SchedulingCSPConstructor():
+
+    def __init__(self, bulletin, profile):
+        """
+        Saves the necessary data.
+
+        @param bulletin: Stanford Bulletin that provides a list of courses
+        @param profile: A student's profile and requests
+        """
+        self.activities = activities # yelp data (based on genre) TODO
+        self.profile = profile # user input
+        self.num_slots = 10
+        self.restaraunts = #TODO
+
+    def add_variables(self, csp):
+        """
+        Adding the variables into the CSP. Each variable, (request, quarter),
+        can take on the value of one of the courses requested in request or None.
+        For instance, for quarter='Aut2013', and a request object, request, generated
+        from 'CS221 or CS246', then (request, quarter) should have the domain values
+        ['CS221', 'CS246', None]. Conceptually, if var is assigned 'CS221'
+        then it means we are taking 'CS221' in 'Aut2013'. If it's None, then
+        we not taking either of them in 'Aut2013'.
+
+        @param csp: The CSP where the additional constraints will be added to.
+        """
+        
+        max_travel_time = 60 #mins
+        time_domain = range(0, max_travel_time + 1) # max travel time
+        # slots (including the travel time)
+        for i in range(0, num_slots):
+            if i == 0:
+                csp.add_variable(((i, "home"), home_domain) #TODO fill home domain with fake activity of money 0 and time 0
+                continue
+            if i % 2 == 0:
+                csp.add_variable((i, "activity"), self.activities + self.restaraunts + [None])
+            else:
+                # travel time
+                csp.add_variable((i, "travel"), time_domain + [None])
+
+    # TODO
+    # add durations for food
+    # add durations for activities
+
+    # constraints
+    # budget: value of (i, "activity").money summed up less than user budget
+    # time: value of (i, "activity").time and (i, "travel") summed up less than user time
+    # travel time: unary factor where for each (i, "travel") if less time, then greater weight and vv
+    # food: sum number of activities that have food, and if they want food it has to equal one or else 0
+    # travel time: ternary factor where for each travel slot, travel_time(prev_activity, next_activity) = travel_time, set (i, "travel") equal to that
+    # rating: for the value of each (i, "activity"), we give a higher weight for a better rating, UNARY FACTOR
+
+    def add_bulletin_constraints(self, csp):
+        """
+        Add the constraints that a course can only be taken if it's offered in
+        that quarter.
+
+        @param csp: The CSP where the additional constraints will be added to.
+        """
+        for request in self.profile.requests:
+            for quarter in self.profile.quarters:
+                csp.add_unary_factor((request, quarter), \
+                    lambda cid: cid is None or \
+                        self.bulletin.courses[cid].is_offered_in(quarter))
+
+    def add_norepeating_constraints(self, csp):
+        """
+        No course can be repeated. Coupling with our problem's constraint that
+        only one of a group of requested course can be taken, this implies that
+        every request can only be satisfied in at most one quarter.
+
+        @param csp: The CSP where the additional constraints will be added to.
+        """
+        for request in self.profile.requests:
+            for quarter1 in self.profile.quarters:
+                for quarter2 in self.profile.quarters:
+                    if quarter1 == quarter2: continue
+                    csp.add_binary_factor((request, quarter1), (request, quarter2), \
+                        lambda cid1, cid2: cid1 is None or cid2 is None)
+
+    def get_basic_csp(self):
+        """
+        Return a CSP that only enforces the basic constraints that a course can
+        only be taken when it's offered and that a request can only be satisfied
+        in at most one quarter.
+
+        @return csp: A CSP where basic variables and constraints are added.
+        """
+        csp = util.CSP()
+        self.add_variables(csp)
+        self.add_bulletin_constraints(csp)
+        self.add_norepeating_constraints(csp)
+        return csp
+
+    def add_quarter_constraints(self, csp):
+        """
+        If the profile explicitly wants a request to be satisfied in some given
+        quarters, e.g. Aut2013, then add constraints to not allow that request to
+        be satisfied in any other quarter.
+
+        @param csp: The CSP where the additional constraints will be added to.
+        """
+        # Problem 3a
+        # BEGIN_YOUR_CODE (our solution is 5 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+    def add_request_weights(self, csp):
+        """
+        Incorporate weights into the CSP. By default, a request has a weight
+        value of 1 (already configured in Request). You should only use the
+        weight when one of the requested course is in the solution. A
+        unsatisfied request should also have a weight value of 1.
+
+        @param csp: The CSP where the additional constraints will be added to.
+        """
+        for request in self.profile.requests:
+            for quarter in self.profile.quarters:
+                csp.add_unary_factor((request, quarter), \
+                    lambda cid: request.weight if cid != None else 1.0)
+
+    def add_prereq_constraints(self, csp):
+        """
+        Adding constraints to enforce prerequisite. A course can have multiple
+        prerequisites. You can assume that *all courses in req.prereqs are
+        being requested*. Note that if our parser inferred that one of your
+        requested course has additional prerequisites that are also being
+        requested, these courses will be added to req.prereqs. You will be notified
+        with a message when this happens. Also note that req.prereqs apply to every
+        single course in req.cids. If a course C has prerequisite A that is requested
+        together with another course B (i.e. a request of 'A or B'), then taking B does
+        not count as satisfying the prerequisite of C. You cannot take a course
+        in a quarter unless all of its prerequisites have been taken *before* that
+        quarter. You should take advantage of get_or_variable().
+
+        @param csp: The CSP where the additional constraints will be added to.
+        """
+        # Iterate over all request courses
+        for req in self.profile.requests:
+            if len(req.prereqs) == 0: continue
+            # Iterate over all possible quarters
+            for quarter_i, quarter in enumerate(self.profile.quarters):
+                # Iterate over all prerequisites of this request
+                for pre_cid in req.prereqs:
+                    # Find the request with this prerequisite
+                    for pre_req in self.profile.requests:
+                        if pre_cid not in pre_req.cids: continue
+                        # Make sure this prerequisite is taken before the requested course(s)
+                        prereq_vars = [(pre_req, q) \
+                            for i, q in enumerate(self.profile.quarters) if i < quarter_i]
+                        v = (req, quarter)
+                        orVar = get_or_variable(csp, (v, pre_cid), prereq_vars, pre_cid)
+                        # Note this constraint is enforced only when the course is taken
+                        # in `quarter` (that's why we test `not val`)
+                        csp.add_binary_factor(orVar, v, lambda o, val: not val or o)
+
+    def add_unit_constraints(self, csp):
+        """
+        Add constraint to the CSP to ensure that the total number of units are
+        within profile.minUnits/maxUnits, inclusively. The allowed range for
+        each course can be obtained from bulletin.courses[cid].minUnits/maxUnits.
+        For a request 'A or B', if you choose to take A, then you must use a unit
+        number that's within the range of A. You should introduce any additional
+        variables that you need. In order for our solution extractor to
+        obtain the number of units, for every requested course, you must have
+        a variable named (courseId, quarter) (e.g. ('CS221', 'Aut2013')) and
+        its assigned value is the number of units.
+        You should take advantage of get_sum_variable().
+
+        @param csp: The CSP where the additional constraints will be added to.
+        """
+        # Problem 3b
+        # Hint 1: read the documentation above carefully
+        # Hint 2: the domain for each (courseId, quarter) variable should contain 0
+        #         because the course might not be taken
+        # Hint 3: use nested functions and lambdas like what get_or_variable and
+        #         add_prereq_constraints do
+        # Hint 4: don't worry about quarter constraints in each Request as they'll
+        #         be enforced by the constraints added by add_quarter_constraints
+
+        # BEGIN_YOUR_CODE (our solution is 16 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+    def add_all_additional_constraints(self, csp):
+        """
+        Add all additional constraints to the CSP.
+
+        @param csp: The CSP where the additional constraints will be added to.
+        """
+        self.add_quarter_constraints(csp)
+        self.add_request_weights(csp)
+        self.add_prereq_constraints(csp)
+        self.add_unit_constraints(csp)
